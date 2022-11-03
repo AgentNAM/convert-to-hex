299bc30cc8d9969964e400674cfd22a12fde794d
Necolai McIntosh <necolai2013@gmail.com>
2022-11-03 13:58:57 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertHex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConvertToHex("hello world"));
+            Console.WriteLine(ConvertToHex("Big Boi"));
+            Console.WriteLine(ConvertToHex("Marty Poppinson"));
+
+            Console.ReadKey();
+        }
+
+        static string ConvertToHex(string str)
+        {
+            string hexStr = "";
+
+            foreach (char c in str)
+            {
+                hexStr += BitConverter.ToString(BitConverter.GetBytes(c)).Trim('0').Trim('-').ToLowerInvariant() + " ";
+            }
+
+            hexStr = hexStr.Trim();
+
+            return hexStr;
+        }
+    }
+}
